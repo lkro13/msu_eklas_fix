0719142237e2182e66ceaa1790dbb37c3d1789b8
Shervain123 <81868987+shervain123@users.noreply.github.com>
2025-01-22 22:59:16 +0800

    Add files via upload

---
+++ inject.js
@@ -0,0 +1,3 @@
+var s = document.createElement('script');
+s.textContent = 'var igt_kok = "yes";function ingat_ingat_lo(lorrr){igt_kok = lorrr};';
+(document.head || document.documentElement).appendChild(s);
